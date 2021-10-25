e7a01bcff30478e27a457af380f64d7fabf92f47
Andy Whetstone <andy_whetstone@live.com>
2021-10-25 16:11:26 -0500

    Generate relation between event and event creator

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_25_210535) do
+ActiveRecord::Schema.define(version: 2021_10_25_211009) do
 
   create_table "events", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2021_10_25_210535) do
     t.datetime "date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
