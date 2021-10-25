de113860ef56e1919782a03e491a52471750b2f6
Andy Whetstone <andy_whetstone@live.com>
2021-10-25 17:03:35 -0500

    Add relation between User and Creator. Created some simple views for showing all events.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_25_211009) do
+ActiveRecord::Schema.define(version: 2021_10_25_212936) do
 
   create_table "events", force: :cascade do |t|
     t.string "title"
@@ -30,8 +30,10 @@ ActiveRecord::Schema.define(version: 2021_10_25_211009) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
