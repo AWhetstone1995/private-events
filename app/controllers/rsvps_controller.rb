0449f7a212bffbe2e02cc4a9f52ed3c858641d6f
Andy Whetstone <andy_whetstone@live.com>
2021-10-26 10:10:13 -0500

    Updated rsvp routing, applied CSS styling across event views.

--- app/controllers/rsvps_controller.rb
+++ app/controllers/rsvps_controller.rb
@@ -1,41 +1,27 @@
 class RsvpsController < ApplicationController
-#   before_action :require_login
-
   def create
-    event = Event.find(params[:event_id])
-    user = current_user
-    @rsvp = Rsvp.new(event_id: event.id, user_id: user.id)
+    # event = Event.find(params[:event_id])
+    # user = current_user
+    @rsvp = Rsvp.new(event_id: params[:event_id], user_id: current_user.id)
 
     if @rsvp.save
       flash[:notice] = "You have succesfully enrolled for the event"
-      redirect_to event_path(event)
+      redirect_to event_path(params[:event_id])
     else
       flash[:alert] = "Something went wrong"
-      render event_path(event)
+      render event_path(params[:event_id])
     end
   end
 
   def destroy
+    # debugger
     event = Event.find(params[:event_id])
-    rsvp = Rsvp.find(params[:id])
+    rsvp = Rsvp.find_by(event_id: event.id, user_id: current_user.id)
     rsvp.destroy
+    flash[:notice] = "You have cancelled this upcoming event."
     redirect_to event_path(rsvp.event)
   end
-  
-  def update
-    event = Event.find(params[:event_id])
-    user = current_user
-    if Rsvp.where(event_id: event.id, user_id: user.id).exists?
-      Rsvp.destroy(Rsvp.where(event_id: event.id, user_id: user.id).first.id)
-    else
-      Rsvp.create(event_id: event.id, user_id: user.id)
-      redirect_to event_path(event)
-    end
-  end
 
-#   def new
-#     @event = current_user.build
-#   end
   private
 
   def enrollment_params
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :users
   resources :events
   post '/rsvps/:id', to: 'rsvps#create'
-  delete '/rsvps/:id', to: 'rsvps#destroy'
+  # delete '/rsvps/:id', to: 'rsvps#destroy'
   resources :rsvps
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'events#index'
