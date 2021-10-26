9b359d526317e6a9fe6584e4e5996fab3e2f05b3
Andy Whetstone <andy_whetstone@live.com>
2021-10-26 00:25:42 -0500

    Added scope for Event, final project completion

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,7 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User', foreign_key: :user_id
   has_many :rsvp, dependent: :destroy
   has_many :attendees, through: :rsvp, source: :user
+
+  scope :past, -> { where("date < ?", Date.current) }
+  scope :upcoming, -> { where("date > ?", Date.current) }
 end
